d3a51a397f2bd3877f4d00bbfea8b4c489c69025
jrsergiogit <jrsergio@hotmail.com>
2026-09-18 19:04:48 -0300

    Fix canonical domain redirect loop

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "dustsweepertool.com",
-          },
-        ],
-        destination: "https://dustsweepertool.com/:path*",
-        permanent: true,
-      },
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.dustsweepertool.com",
-          },
-        ],
-        destination: "https://dustsweepertool.com/:path*",
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
